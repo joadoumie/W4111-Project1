9a97e694c3e48e9a810df8e9d10988ffc0259634
Solomon Chang <sjc2233@columbia.edu>
2022-04-13 16:00:35 -0400

    all recipe info in progress

--- webserver/server.py
+++ webserver/server.py
@@ -1,4 +1,3 @@
-
 """
 Columbia W4111 Intro to databases
 Example webserver
@@ -20,25 +19,22 @@ from sqlalchemy import *
 from sqlalchemy.pool import NullPool
 from flask import Flask, request, render_template, g, redirect, Response
 
-
-#JSON RECIPEUID STUFF
+# JSON RECIPEUID STUFF
 with open('ids.json') as f:
-  ids_data = json.load(f)
+    ids_data = json.load(f)
 
 print(ids_data)
 print(ids_data["recipeuid"])
 recipe_uid = ids_data["recipeuid"]
-#ids_data["recipeuid"] += 1
+# ids_data["recipeuid"] += 1
 print(ids_data["recipeuid"])
 with open('ids.json', 'w') as json_file:
-  json.dump(ids_data, json_file)
+    json.dump(ids_data, json_file)
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
 
-
-
-# XXX: The Database URI should be in the format of: 
+# XXX: The Database URI should be in the format of:
 #
 #     postgresql://USER:PASSWORD@<IP_OF_POSTGRE_SQL_SERVER>/<DB_NAME>
 #
@@ -54,14 +50,12 @@ DB_PASSWORD = "bridge"
 
 DB_SERVER = "w4111.cisxo09blonu.us-east-1.rds.amazonaws.com"
 
-DATABASEURI = "postgresql://"+DB_USER+":"+DB_PASSWORD+"@"+DB_SERVER+"/proj1part2"
-
+DATABASEURI = "postgresql://" + DB_USER + ":" + DB_PASSWORD + "@" + DB_SERVER + "/proj1part2"
 
 #
 # This line creates a database engine that knows how to connect to the URI above
 #
 engine = create_engine(DATABASEURI)
-
 
 # Here we create a test table and insert some values in it
 engine.execute("""DROP TABLE IF EXISTS test;""")
@@ -72,33 +66,34 @@ engine.execute("""CREATE TABLE IF NOT EXISTS test (
 engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
 
 
-
 @app.before_request
 def before_request():
-  """
+    """
   This function is run at the beginning of every web request 
   (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request
 
   The variable g is globally accessible
   """
-  try:
-    g.conn = engine.connect()
-  except:
-    print ("uh oh, problem connecting to database")
-    import traceback; traceback.print_exc()
-    g.conn = None
+    try:
+        g.conn = engine.connect()
+    except:
+        print("uh oh, problem connecting to database")
+        import traceback;
+        traceback.print_exc()
+        g.conn = None
+
 
 @app.teardown_request
 def teardown_request(exception):
-  """
+    """
   At the end of the web request, this makes sure to close the database connection.
   If you don't the database could run out of memory!
   """
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
+    try:
+        g.conn.close()
+    except Exception as e:
+        pass
 
 
 #
@@ -116,7 +111,7 @@ def teardown_request(exception):
 #
 @app.route('/')
 def index():
-  """
+    """
   request is a special object that Flask provides to access web request information:
 
   request.method:   "GET" or "POST"
@@ -126,53 +121,42 @@ def index():
   See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
   """
 
-  # DEBUG: this is debugging code to see what request looks like
-  print (request.args)
+    # DEBUG: this is debugging code to see what request looks like
+    print(request.args)
+
+    #
+    # example of a database query
+    #
+    cursor1 = g.conn.execute("SELECT * FROM recipes")
+    recipeIDs = []
+    recipeNames = []
+    recipeIns = []
+    for result in cursor1:
+        recipeIDs.append(result['recipeid'])  # can also be accessed using result[0]
+        recipeNames.append(result['recipename'])
+        recipeIns.append(result['instructions'])
+    cursor1.close()
+
+    for curr_recipe in recipeNames:
+        temp_sql = """
+        select name
+        from recipes natural join contains_ingredients natural join ingredients
+        where recipename = %s
+        """ % (curr_recipe)
+        tempCursor = g.conn.execute(temp_sql)
 
 
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
+    print(sql1)
+    #cursor2 = g.conn.execute()
 
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
+    context1 = dict(data1=recipeIDs, data2=recipeNames, data3=recipeIns)
 
+    #
+    # render_template looks in the templates/ folder for files.
+    # for example, the below file reads template/index.html
+    #
+    return render_template("index.html", **context1)
 
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at
@@ -184,34 +168,37 @@ def index():
 #
 @app.route('/another')
 def another():
-  return render_template("anotherfile.html")
+    return render_template("anotherfile.html")
+
 
 @app.route('/home')
 def recipe_home():
-  return render_template("recipe_home.html")
+    return render_template("recipe_home.html")
+
 
 @app.route('/testnav')
 def navbar():
-  return render_template("testnavbar.html")
+    return render_template("testnavbar.html")
 
 
 # Example of adding new data to the database
 @app.route('/add', methods=['POST'])
 def add():
-  name = request.form['name']
-  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-  g.conn.execute(text(cmd), name1 = name, name2 = name);
-  return redirect('/')
+    name = request.form['name']
+    cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
+    g.conn.execute(text(cmd), name1=name, name2=name);
+    return redirect('/')
+
 
 @app.route('/addrecipe', methods=['POST'])
 def recipe_add():
-  recipe_name = request.form['recipename']
-  instructions = request.form['instructions']
-  ingredients = request.form['ingredients']
-  print (recipe_name, instructions, ingredients)
-  cmd = 'INSERT INTO recipes VALUES (:name1, :name2, :name3)';
-  g.conn.execute(text(cmd), name1 = recipe_uid, name2 = recipe_name, name3 = instructions);
-  return redirect('/home')
+    recipe_name = request.form['recipename']
+    instructions = request.form['instructions']
+    ingredients = request.form['ingredients']
+    print(recipe_name, instructions, ingredients)
+    cmd = 'INSERT INTO recipes VALUES (:name1, :name2, :name3)';
+    g.conn.execute(text(cmd), name1=recipe_uid, name2=recipe_name, name3=instructions);
+    return redirect('/home')
 
 
 @app.route('/login')
@@ -221,15 +208,16 @@ def login():
 
 
 if __name__ == "__main__":
-  import click
+    import click
 
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
+
+    @click.command()
+    @click.option('--debug', is_flag=True)
+    @click.option('--threaded', is_flag=True)
+    @click.argument('HOST', default='0.0.0.0')
+    @click.argument('PORT', default=8111, type=int)
+    def run(debug, threaded, host, port):
+        """
     This function handles command line parameters.
     Run the server using
 
@@ -241,9 +229,9 @@ if __name__ == "__main__":
 
     """
 
-    HOST, PORT = host, port
-    print ("running on %s:%d" % (HOST, PORT))
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+        HOST, PORT = host, port
+        print("running on %s:%d" % (HOST, PORT))
+        app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
 
-  run()
+    run()
